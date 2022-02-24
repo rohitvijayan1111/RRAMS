6aca1263a2a0189ef884d24fd9d128a2aa728bfc
rohitvijayan1111 <97788571+rohitvijayan1111@users.noreply.github.com>
2022-02-24 13:05:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-# This is a sample Python script.
+import tkinter
+main=tkinter.Tk()
+main.title("Voice assistant")#gives name to the interface
+main.geometry("803x499") # defines the geometry of the interface
+bg_img=tkinter.PhotoImage(file="background3.png") #creates an object for storing image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bg_canvas=tkinter.Canvas(main,width=336,height=450,bd=0,highlightthickness=0)
+bg_canvas.pack(fill="both",expand=True)
+bg_canvas.create_image(0,0,image=bg_img,anchor="nw")
+def one():
+    import voice_assistant
+def two():
+    import keyboard
+#voie_back=tkinter.PhotoImage(main, file="voice_logo.png") #creates an object for storing image
+#label2=tkinter.PhotoImage(main,image=voice_back)
+#label2.place(x=190,y=152)
+assistant_img=tkinter.PhotoImage(file="assistant-pic.png") #creates an object for storing image
+assistant_button=tkinter.Button(main,image=assistant_img,borderwidth=0,width=174,height=187,fg="black",command=one)
+assistant_button.place(x=103,y=55)
+delay=5000
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+piano_img=tkinter.PhotoImage(file="piano-pic.png") #creates an object for storing image
+piano_button=tkinter.Button(main,image=piano_img,borderwidth=0,width=183,height=187,fg="black",command=two)
+piano_button.place(x=535,y=55)
 
+decibel_img=tkinter.PhotoImage(file="decibel-pic.png") #creates an object for storing image
+decibel_button=tkinter.Button(main,image=decibel_img,borderwidth=0,width=186,height=187,fg="black")
+decibel_button.place(x=92,y=249)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+clap_img=tkinter.PhotoImage(file="clap clap pic.png") #creates an object for storing image
+clap_button=tkinter.Button(main,image=clap_img,borderwidth=0,width=183,height=184,fg="black")
+clap_button.place(x=533,y=251)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("hi")
+# Code to add widgets will go here...
+#import app_locations
+
+main.mainloop()
