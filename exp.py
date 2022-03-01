49c1fb40400cd58554fad34e6c9dbecf74dd2e7d
anshulcs <97788560+anshulcs@users.noreply.github.com>
2022-03-02 00:28:48 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,38 +1,82 @@
-import tkinter
-main=tkinter.Tk()
-main.title("Voice assistant")#gives name to the interface
-main.geometry("803x499") # defines the geometry of the interface
-bg_img=tkinter.PhotoImage(file="background3.png") #creates an object for storing image
+import tkinter #for UI
+import datetime #for time
+import speech_recognition
+import playsound
+import pyttsx3
+from threading import *
+ #control and monitor input devices
+window= tkinter.Tk()
+#window= tkinter.Toplevel()
+window.title("RRAMS")#gives name to the interface
+window.geometry("803x499") # defines the geometry of the interface
+x=42
+y=80
+#creates an object for storing image
+#bg_label=tkinter.Label(window,image=bg_img)
+#bg_label.place(x=0,y=0)
 
-bg_canvas=tkinter.Canvas(main,width=336,height=450,bd=0,highlightthickness=0)
+def output_text(texts):
+    global x,y
+    if len(texts) <32:
+        bg_canvas.create_text(x,y,text=texts,font=('Georgia',14,"italic"),fill='white',anchor=tkinter.NW)
+        y = y + 20
+    else:
+        x=40
+        text_list=texts.split()
+        length=0
+        str=""
+        str2=""
+        z=0
+        change(texts)
+
+    #x=x+30
+def change(texts):
+    global x,y,z
+    text_list = texts.split()
+    str = ""
+    str2 = ""
+    z=0
+    length = 0
+    while z < len(text_list)-1 and length < 32:
+        length = length + len(text_list[z]) + 1
+        str = str + " " + text_list[z]
+        z = z + 1
+    else:
+        for b in text_list[z:]:
+            str2 = str2 + " " + b
+    bg_canvas.create_text(x, y, text=str, font=('Georgia',14,"italic"), fill='white', anchor=tkinter.NW)
+    y=y+20
+
+    if len(str2)<32:
+        bg_canvas.create_text(x, y, text=str2, font=('Georgia',14,"italic"), fill='white', anchor=tkinter.NW)
+    else:
+        change(str2)
+    y=y+20
+
+def text_to_speech(text1) :
+    voice_obj=pyttsx3.init()
+    voices=voice_obj.getProperty("voices")
+    voice_obj.setProperty('voice',voices[0].id)
+    voice_obj.setProperty("rate",160)
+    voice_obj.say(text1)
+    voice_obj.runAndWait()
+
+def one():
+    window.destroy()
+def sptx():
+    output_text("hello")
+    text_to_speech("hello")
+def thread():
+    Thread(target=output_text("hello this is rrams")).start()
+    Thread(target=playsound.playsound("C:\\Users\\91984\\Downloads\\sound.mp3"))
+    Thread(target=text_to_speech("hello this is rrams")).start()
+    button_2.destroy()
+bg_img=tkinter.PhotoImage(file="C:\\Users\\91984\\Downloads\\bg (1).png")
+bg_canvas=tkinter.Canvas(window,bg="#000d3c",width=336,height=450,bd=0,highlightthickness=0)
 bg_canvas.pack(fill="both",expand=True)
 bg_canvas.create_image(0,0,image=bg_img,anchor="nw")
-def one():
-    import voice_assistant
-def two():
-    import keyboard
-#voie_back=tkinter.PhotoImage(main, file="voice_logo.png") #creates an object for storing image
-#label2=tkinter.PhotoImage(main,image=voice_back)
-#label2.place(x=190,y=152)
-assistant_img=tkinter.PhotoImage(file="assistant-pic.png") #creates an object for storing image
-assistant_button=tkinter.Button(main,image=assistant_img,borderwidth=0,width=174,height=187,fg="black",command=one)
-assistant_button.place(x=103,y=55)
-delay=5000
-
-
-piano_img=tkinter.PhotoImage(file="piano-pic.png") #creates an object for storing image
-piano_button=tkinter.Button(main,image=piano_img,borderwidth=0,width=183,height=187,fg="black",command=two)
-piano_button.place(x=535,y=55)
-
-decibel_img=tkinter.PhotoImage(file="decibel-pic.png") #creates an object for storing image
-decibel_button=tkinter.Button(main,image=decibel_img,borderwidth=0,width=186,height=187,fg="black")
-decibel_button.place(x=92,y=249)
-
-clap_img=tkinter.PhotoImage(file="clap clap pic.png") #creates an object for storing image
-clap_button=tkinter.Button(main,image=clap_img,borderwidth=0,width=183,height=184,fg="black")
-clap_button.place(x=533,y=251)
-
-# Code to add widgets will go here...
-#import app_locations
-
-main.mainloop()
+button_1=tkinter.Button(window,text="Next",borderwidth=0,font=('Georgia',10,"italic","bold"),width=5,bg="#060c40",height=2,fg="white",command=one)
+button_1.place(x=670,y=400)
+button_2=tkinter.Button(window,text="ARE YOU READY TO BEGIN YOUR MUSIC JOURNEY",borderwidth=0,font=('Georgia',10,"italic","bold"),bg="#060c40",fg="white",command =thread)
+button_2.place(x=200,y=240)
+window.mainloop()
